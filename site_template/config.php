e57802bc718f06bc8c5ee8e26e8bb74566b84577
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-07-28 14:26:48 +1200

    a

---
+++ site_template/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', "on"); // change to 'Off' when live
+
+define ("DB_NAME","ayerst9198_L2_Prac");
+define ("DB_USERNAME", "ayerst9198");
+define ("DB_PASSWORD", "K78BymGzzDLIpyJL");
+define ("DB_HOST", "localhost");
+
+?>
